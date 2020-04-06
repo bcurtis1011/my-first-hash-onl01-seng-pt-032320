bd3b65fa6f47369618f42414b7aa099ed844c97b
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-04-06 01:27:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {cat
+  my_hash = {"dog" ==> "ellie"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
